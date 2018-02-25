be1ee0f6d42521b29d5e6a13cbcf90f8ad075ad0
coffman21 <fire2low@rambler.ru>
2018-02-25 04:00:56 +0300

    sql queries update

--- src/main/sql/postgres-create-database.sql
+++ src/main/sql/postgres-create-database.sql
@@ -24,10 +24,14 @@ create table tusovka (
 
 -- here are some test insertions
 
-INSERT INTO place (name, address, description) VALUES ('Стёпик', 'Москва, ул. Пушкина, д. 2', 'Просто хорошее место для отдыха.');
-INSERT INTO place (name, address, description) VALUES ('Powerhouse', 'Москва, около Таганской', 'Здесь играет музыка.');
+INSERT INTO place (name, address, description, latitude, longitude) VALUES ('Стёпик', 'Москва, ул. Пушкина, д. 2', 'Просто хорошее место для отдыха.', 37.6480051, 37.6480051);
+INSERT INTO place (name, address, description, latitude, longitude) VALUES ('Powerhouse', 'Москва, около Таганской', 'Здесь играет музыка.', 54.7177024, 37.6480051);
+INSERT INTO place (name, address, description, latitude, longitude) VALUES ('test_place1', 'Берлин', 'Берлин', 52.524063, 13.361528);
+INSERT INTO place (name, address, description, latitude, longitude) VALUES ('test_place2', 'МГТУ им. Н.Э.Баумана, 2-я Бауманская улица, Москва', 'Бомонка', 55.765900, 37.685198);
+INSERT INTO place (name, address, description, latitude, longitude) VALUES ('test_place3', 'Олимпийский проспект, 13А, Мытищи', 'Мытищи ебаные', 55.918597,37.763928);
 
 insert INTO tusovka (name, description, date, price, place) VALUES ('asd', 'adad', '2005-08-09T18:31:42', 100, 'Powerhouse');
 
 select * from place;
 select * from tusovka;
+select * from message_log;
